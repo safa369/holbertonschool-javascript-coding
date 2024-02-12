b0a606815cac1e287dc53f0bdf4ce88d35335684
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-12 14:38:12 +0100

    write

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,7 +2,8 @@
 
 const fs = require('fs');
 const { argv } = require('process');
-fs.appendFile(argv[2], argv[3], (err) => {
+fs.appendFile(argv[2], argv[3], () => {});
+fs.readFile(argv[2], 'utf8', (err, data) => {
   if (err) throw err;
-  console.log();
+  console.log(data);
 });
