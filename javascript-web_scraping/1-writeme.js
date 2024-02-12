23fbf5bc2b52375aa053def467072984683cf9db
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-12 14:31:37 +0100

    write

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const { argv } = require('process');
-fs.writeFile(argv[2], argv[3], (err, data) => {
+fs.writeFile(argv[2], argv[3], (err) => {
   if (err) throw err;
-  console.log(data);
+  console.log();
 });
