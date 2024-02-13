9285346ec3a2be94a1be2ea0d29fc4176fa2e455
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-02-13 11:19:25 +0100

    write

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,9 +3,8 @@
 const fs = require('fs');
 const filep = process.argv[2];
 const msg = process.argv[3];
-fs.writeFile(filep, 'utf8', (err) => {
+fs.writeFile(filep, msg, 'utf8', (err) => {
   if (err) {
-  console.log(data);
-}
+    console.log(err);
+  }
 });
-
