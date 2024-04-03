1f77407808dd5b10b7babf015e13e21695c37072
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-03 13:13:45 +0200

    stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
-  console.log('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-  process.stdin.on('readable', () => {
-    const name = process.stdin.read();
-    if (name !== null) {
-      console.log(`Your name is: ${name.toString().trim()}`);
-    }
-  });
-  process.on('exit', () => {
-    console.log('This important software is now closing\n');});
-
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    console.log(`Your name is: ${name.toString().trim()}`);
+  }
+});
+process.on('exit', () => {
+  console.log('This important software is now closing\n');
+});
